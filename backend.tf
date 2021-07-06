d70a15e7642b91d5ee1ae75b6b54e610153416bc
RushiSheth <rushi1sheth@ymail.com>
2021-07-06 02:28:02 -0400

    backend deleted

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-    backend "azurerm"{
-        resource_group_name = "tfstateRG"
-        storage_account_name = "tfstatelab7557"
-        container_name = "tfstatefiles"
-        key = "pDMRbsfb/pBKaKwjf+HgF5gFQl6rSdcTltJ6F4g7O1ExMuD/y73WFZNkiNhutTchMgFQlFFQFT4CInysZSbmgg=="
-    }
-}
